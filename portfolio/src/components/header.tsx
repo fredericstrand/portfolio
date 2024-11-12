fb9c112ec5bb2d76294b563c36be489485d1c148
fredericstrand <strandfrederic@outlook.com>
2024-11-12 16:01:24 +0100

    first commit

---
+++ portfolio/src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <nav>
+            <div className="bg-stone-50 p-10">
+                <div className="container mx-auto justify-items-center items-center">
+                    a.
+                </div>
+            </div>
+        </nav>
+    );
+}
